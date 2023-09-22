724500c1e33f86103af2e9f4cf1832b3d06df974
Dylan <dylan.reeder5@gmail.com>
2023-09-22 16:52:01 -0400

    update jest

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,7 +1,24 @@
 import parseXML from '../src/parser';
 
 test('parseXML should return parsed object', async () => {
-  const xml = `<root><element>value</element></root>`;
-  const result = await parseXML(xml);
-  expect(result).toEqual({root: {element: ['value']}});
+  const xml = `
+        <config>
+        <subdomains>
+            <subdomain>http://example.com</subdomain>
+            <subdomain>http://test.com</subdomain>
+        </subdomains>
+        <cookies>
+            <cookie name="cookie1" host="http://example.com">value1</cookie>
+            <cookie name="cookie2" host="http://test.com">value2</cookie>
+        </cookies>
+        </config>
+    `;
+
+  const {subdomains, cookies} = await parseXML(xml);
+
+  expect(subdomains).toEqual(['http://example.com', 'http://test.com']);
+  expect(cookies).toEqual([
+    {name: 'cookie1', host: 'http://example.com', value: 'value1'},
+    {name: 'cookie2', host: 'http://test.com', value: 'value2'},
+  ]);
 });
